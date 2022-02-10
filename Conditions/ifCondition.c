4165636e01805eeedc698e1d3bc8d5db9f00ce1a
Michael B.Kh <michaelkhany@gmail.com>
2022-02-10 17:36:56 +0300

    Create ifCondition.c

---
+++ Conditions/ifCondition.c
@@ -0,0 +1,14 @@
+//Long version of if condition
+int time = 20;
+if (time < 18) {
+  cout << "Good day.";
+} else {
+  cout << "Good evening.";
+}
+
+//Short version of if condition
+// variable = (condition) ? expressionTrue : expressionFalse;
+int time = 20;
+string result = (time < 18) ? "Good day." : "Good evening.";
+cout << result;
+
